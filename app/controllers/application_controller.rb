8d87e91594a7fb43c8075546f6822deea89502de
Jared Harbison <jared.harbison@gmail.com>
2019-03-09 20:55:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     @user = User.new(:username => params[:username], :password => params[:password])
-    
+
     if @user.username != "" && @user.password != ""
       redirect '/login'
     else
@@ -38,8 +38,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     @user = User.find_by(:username => params[:username])
-    
-    if @user && @user.authenticate(params[:password]) 
+
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect "/account"
     else
